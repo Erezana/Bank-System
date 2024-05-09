52034b39977dda0b0d68de40d1e3ce4bf4c8b914
Erezana <erzanaberisha2001@gmail.com>
2024-05-09 16:11:11 +0200

    now the user cant open two accounts with the same id number

--- Bank.java
+++ Bank.java
@@ -20,6 +20,12 @@ public class Bank {
             System.out.println("Account ID must be a 4-digit number.");
             return;
         }
+        for (Account account : accounts) {
+            if (account.getAccountId() == accountId) {
+                System.out.println("Account with ID " + accountId + " already exists. Please choose a different ID.");
+                return;
+            }
+        }
         Account account = new Account(accountId, userName);
         accounts.add(account);
         System.out.println("Account created successfully");
