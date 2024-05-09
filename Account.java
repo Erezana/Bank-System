ec8b7a3a9225c25d435465a89676fa8993b8b230
Erezana <erzanaberisha2001@gmail.com>
2024-05-09 14:37:06 +0200

    created three classes: account, transaction and bank with their attributes and constructors as requested

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    private int accountId;
+    private String userName;
+    private double balance;
+
+    public Account(int accountId, String userName) {
+        this.accountId= accountId;
+        this.userName=userName;
+        this.balance=0.0;
+    }
+
+}
---
+++ Bank.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Bank {
+    private String bankName;
+    private List<Account> accounts;
+    private double totalTransactionFeeAmount;
+    private double totalTransferAmount;
+    private double transactionFlatFeeAmount;
+    private double transactionPercentFeeValue;
+
+    public Bank(String bankName, double transactionFlatFeeAmount, double transactionPercentFeeValue){
+        this.bankName = bankName;
+        this.transactionFlatFeeAmount = transactionFlatFeeAmount;
+        this.transactionPercentFeeValue = transactionPercentFeeValue;
+        this.accounts = new ArrayList<>();
+        this.totalTransactionFeeAmount = 0.0;
+        this.totalTransferAmount = 0.0;
+    }
+
+}
